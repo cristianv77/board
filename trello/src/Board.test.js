7481adf16a5313f5c4faedb4d3d82b8ad9a250fe
cristianv77 <vargas.t.cristian@gmail.com>
2019-12-06 17:47:51 -0300

    Update Board.test.js

--- trello/src/Board.test.js
+++ trello/src/Board.test.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import Board from './Board';
 import {initializeColumns, initializeCards } from './helpers/Initialize'
 
-it('renders', () => {
+it('renders and archives', () => {
   initializeColumns();
   initializeCards();
-  const board = document.createElement('board');
-  ReactDOM.render(<Board />, board);
+  const element = document.createElement('board');
+  const board = ReactDOM.render(<Board />, element);
+  board.archiveColumn('To do');
+  console.log(board.state.columns);
 });
